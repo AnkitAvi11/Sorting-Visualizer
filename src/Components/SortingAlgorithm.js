869973c6041df42c5f4955d9ab45e7c18c10bb08
Ankit Kumar <kumar.ankit383@gmail.com>
2020-10-18 10:16:29 +0530

    Made some minor ui changes to make it smooth

--- src/Components/SortingAlgorithm.js
+++ src/Components/SortingAlgorithm.js
@@ -18,8 +18,14 @@ const bubbleSort = (arr, auxarray, animations) => {
         for(let j =0;j<arr.length;j++){
             if(arr[j] > arr[j+1]) {
                 animations.push([j,j+1]);
-                animations.push([j,j+1]);
-                animations.push([[j,arr[j+1]],[j+1,arr[j]]]);
+                animations.push([j+1,j]);
+                animations.push(
+
+                    [[j,arr[j+1]],
+
+                    [j+1,arr[j]]]
+                    
+                );
                 let temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
